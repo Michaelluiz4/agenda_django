0aaf79512f34e91d25f88e81f76a6c6826bf3796
Michael <michaelsb065@gmail.com>
2024-11-19 01:57:51 -0300

    Modificação

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',    # nomes do display
+    ordering = '-id', # ordenação da lista (estou ordenando em ordem descrecente pelo id por ex, mas poderia usar outro meio de ordenação)
+    list_filter = 'date',  # criar um filtro, estou criando um filtro de data por ex
+    search_fields = 'id', 'first_name', 'last_name', # campo de busca
+    list_per_page = 10 # itens que aparecerão por página
+    list_max_show_all = 200 # limite máximo de itens por página
+    # list_editable = 'first_name',
+    list_display_links = 'id', 'first_name' # onde fica o link estou colocando no id e first_name por ex
--- contact/models.py
+++ contact/models.py
@@ -6,9 +6,11 @@ from django.utils import timezone
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
-    phone = models.CharField(max_length=11, blank=True)
+    phone = models.CharField(max_length=11)
     email = models.EmailField(max_length=254, blank=True)
     date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
 
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
